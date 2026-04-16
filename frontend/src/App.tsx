fd83a75d1b81b13ac3ba11c55dedcf224892d7e6
harshithareddy123456 <vsharshi233@gmail.com>
2026-04-16 23:10:26 +0530

    node modules fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import TaskBoard from "./components/TaskBoard.tsx";
-import TaskForm from "./components/TaskForm.tsx";
+import TaskBoard from "./components/TaskBoard";
+import TaskForm from "./components/TaskForm";
 import { fetchTasks } from "./store/slices/taskSlice";
 import { openModal } from "./store/slices/uiSlice";
 import { RootState, AppDispatch } from "./store";
